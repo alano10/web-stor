afae33965bc4c6084d0b3bb468e8db1a8c7aca1e
alano10 <alanoleider@gmail.com>
2019-03-02 04:56:04 -0800

    test web

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include_once 'header.php'
+?>
+<title>Home</title>
+<div class="content">
+<center>
+<img src="img/banner.png"></p>
+<a href="GAMES/">Games</a></p>
+<a href="mods/">Mods</a>
+</center>
+</div>
+</div>
+<?php
+include_once 'footer.php'
+?>
